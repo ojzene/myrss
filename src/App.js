a0bacbd9eeb350f5d22930906b0fc4a4e3606cce
Owolabi Oyekanmi <ttconfirmed@gmail.com>
2021-07-19 07:56:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-
 import React from "react";
 import NavBar from "./components/NavBar";
-
-// import Settings from "./components/Settings";
 import ShowRss from "./components/ShowRss";
-
 import { SettingsProvider } from "./contexts/SettingsContext";
 
 export default function App() {
@@ -17,7 +13,6 @@ export default function App() {
         <div className="container">
           <Switch>
             <Route path="/" exact component={ShowRss}></Route>
-            {/* <Route path="/settings" component={Settings}></Route> */}
           </Switch>
         </div>
       </div>

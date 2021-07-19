7ca54b9f5ff6dd6e06d635072ee56eeaec44a487
Owolabi Oyekanmi <ttconfirmed@gmail.com>
2021-07-19 07:57:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "https://kanmitrssserver.herokuapp.com/graphql",
   cache: new InMemoryCache()
 });
-
-
 const brand = "My RSS Feed"
-
 const reactStrict = (
   <ApolloProvider client={client}>
     <React.StrictMode>
@@ -24,10 +19,6 @@ const reactStrict = (
     </React.StrictMode>
   </ApolloProvider>
 )
-
 ReactDOM.render(reactStrict, document.getElementById('kanmit'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
